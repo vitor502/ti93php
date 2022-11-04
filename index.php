146da5dce41688eef741403dde8e57aec2d8a336
Vitor Fernandes <vitorfs502@gmail.com>
2022-11-04 15:36:20 -0300

    formulario

--- index.php
+++ index.php
@@ -1,26 +1,42 @@
 <?php 
+//Declaração de variaveis, matrizes e manipulação de data/hr manipulação de 
+// - definindo o timezone -
+date_default_timezone_set('America/Sao_Paulo');
+$nome = "vitorfs";
+$dataNasc = date('24/02/2004');
+echo($nome.' - '. $dataNasc);
+echo('<br>');
+$hoje = date ('d/m/Y H:i:s');
+echo($hoje. '<br>');
+echo('<br>');
+$data = new DateTime();
+echo('<br>');
+//print_r($data);
 
-    $times = array();
-    $times[0] = "paumeiras";
-    $times[1] = "paumeira";
-    $times[2] = "paumeir";
+$teste = true;
+$idade = 35;
+$valor = 458.65;
 
+$nome = $_POST['valor-txt'];
+echo('<h1>'. $nome. '<h1>');
 
-  echo("<br>");
-  print_r($times);
-  echo("<br>");
-  foreach ($times as $time){
-    echo($time. "<br>");
-  }
+//Estrutura de controle de decisão
 
+//Estrutura de repetição
+
+//Funções e configuração
 ?>
-<table>
-    <th>Id</th>
-    <th>Nome</th>
-    <?php foreach ($times as $id => $nome) { ?>
-        <tr>
-            <td><?php echo($id + 1);?></td>
-            <td><?php echo($nome);?></td>
-        </tr>
-    <?php } ?>
-</table>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <!-- <meta http-equiv="refresh" content="30"> -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="#" method="post"><input type="text" name="valor-txt" placeholder="digite o valor" id="">
+    <button type="submit">enviar</button>
+  </form>
+</body>
+</html>
